d1fc1371f77247b1849ca88115e61570007edaf0
Daniel Choi <dhchoi@gmail.com>
2012-01-29 18:17:23 -0500

    color config

---
+++ lib/color_config.rb
@@ -0,0 +1,15 @@
+
+class ColorConfig
+  def initialize
+    @dict = {
+      'context' =>  "ECECEC",
+      'project' => "DC143C",
+      '@cambridgeport' => '9400D3',
+      'priority' => 'FFFF00'
+    }
+  end
+
+  def rgb(key)
+    @dict[key] && "RGB_" + @dict[key].upcase.scan(/[A-F0-9]{2}/).join
+  end
+end
--- lib/colorizer.rb
+++ lib/colorizer.rb
@@ -1,4 +1,7 @@
 #!/usr/bin/env ruby
+
+require File.join(File.dirname(__FILE__), 'color_config')
+
 gem 'highline', '>= 1.6.11'
 require 'highline'
 
@@ -16,14 +19,26 @@ terr = HighLine.new(STDIN, STDERR)
 
 filter = ARGV.first
 
-CONTEXT_COLOR = 'CYAN'
-PROJECT_COLOR = 'RED'
+COLORS = ColorConfig.new
 
 def colorize s
   return s if $no_color
   # color @contexts and +projects
-  s.gsub(/@\S+/) {|m| "<%= color '#{m}', #{CONTEXT_COLOR} %>" }.
-    gsub(/\+[\S]+/) {|m| "<%= color '#{m}', #{PROJECT_COLOR} %>" }
+
+  s.gsub(/@\S+/) {|m| 
+      if COLORS.rgb(m)
+        "<%= color '#{m}', #{COLORS.rgb(m)} %>" 
+      else
+        "<%= color '#{m}', #{COLORS.rgb('context')} %>" 
+      end
+    }.
+    gsub(/\+[\S]+/) {|m| 
+      if COLORS.rgb(m)
+        "<%= color '#{m}', #{COLORS.rgb(m)} %>" 
+      else
+        "<%= color '#{m}', #{COLORS.rgb('project')} %>" 
+      end
+    }
 end
 
 def mark_priority s
@@ -31,7 +46,10 @@ def mark_priority s
   return s unless  s =~ /!/ 
   s.chomp!
   erb_re =  Regexp.new "<%=.+%>"
-  style = s =~ /!!!/ ?  ':blink, :warning' : ':warning' 
+  style = COLORS.rgb('priority')
+  if s =~ /!!!/ 
+    style = ":blink, #{style}"
+  end
   s.split(erb_re).map {|a|
     [a, "<%= color '#{a}', #{style} %>"]
   }.each {|(old, new)|
