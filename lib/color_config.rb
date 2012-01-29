7ce2cdf22bc57ac81fd83f7ff0834c941c61ea5d
Daniel Choi <dhchoi@gmail.com>
2012-01-29 18:45:36 -0500

    read colors from yaml file

--- lib/color_config.rb
+++ lib/color_config.rb
@@ -1,19 +1,29 @@
+require 'yaml'
 
 class ColorConfig
+  FILE = "colors.yml"
+
   def initialize
     @dict = {
       'context' =>  "ECECEC",
       'project' => "DC143C",
-      '@cambridgeport' => '9400D3',
       'priority' => 'FFFF00'
     }
+    if File.exist?(FILE)
+      @dict.merge!(YAML::load(File.read(FILE)))
+    end
+  end
+
+  def raw(key)
+    s = @dict[key].to_s.sub(/^#/, '').upcase
+    s.length == 3 ? (s + s) : s
   end
 
   def rgb(key)
-    @dict[key] && "RGB_" + @dict[key].upcase.scan(/[A-F0-9]{2}/).join
+    @dict[key] && "RGB_" + raw(key).scan(/[A-F0-9]{2}/).join
   end
 
   def html(key)
-    @dict[key] && "#" + @dict[key].upcase.scan(/[A-F0-9]{2}/).join
+    @dict[key] && "#" + raw(key).scan(/[A-F0-9]{2}/).join
   end
 end
